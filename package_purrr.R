211a93135f9faad9331467687f8ec7c8b78aacda
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-03-13 23:18:41 -0300

    Add files via upload

---
+++ package_purrr.R
@@ -0,0 +1,41 @@
+# Pacote purrr - Tidyverse -----------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 13/03/2022 -------------------------------------------------------------------------------------------------------------------------
+# Fonte: Associação Brasileira de Jurimetria e Curso R -------------------------------------------------------------------------------------
+
+# Conceito do pacote purrr -----------------------------------------------------------------------------------------------------------------
+
+## Ao usar programação funcional (PF) podemos criar códigos concisos e “pipeáveis”, 
+## que tornam o código mais legível e o processo de debug mais simples. Apesar de o 
+## R base já ter funções que podem ser consideradas elementos de PF, a implementação 
+## destas não é elegante. 
+
+# Iterações básicas ------------------------------------------------------------------------------------------------------------------------
+
+## As funções map() são quase como substitutas para laços for: elas abstraem a iteração 
+## em apenas uma linha. Veja esse exemplo de laço usando for:
+
+soma_um <- function(x) x + 1
+obj <- 10:15
+
+for (i in seq_along(obj)) {
+  obj[i] <- soma_um(obj[i])
+}
+obj
+
+soma_um(3)
+
+## O que de fato estamos tentando fazer com o laço acima? Temos um vetor (obj) e 
+## queremos aplicar uma função (soma_um()) em cada elemento dele. A função map() 
+## remove a necessidade de declaramos um objeto iterador auxiliar (i) e simplesmente 
+## aplica a função desejada em cada elemento do objeto dado.
+
+## Mas antes é necessário baixar o pacote purrr
+
+library(purrr)
+
+soma_um <- function(x) x + 1
+obj <- 10:15
+
+obj <- map(obj, soma_um)
+obj
